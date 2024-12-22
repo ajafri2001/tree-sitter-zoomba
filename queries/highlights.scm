e9268a9070dd3a8d9979efcc1023ce36e471e3ad
ajafri2001 <abdullahjafri2001@gmail.com>
2024-12-23 05:19:44 +0530

    Added syntax highlighting (beta)

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,1 +1,22 @@
-#Todo
+[
+  "return"
+  "assert" 
+  "panic"
+  "import"
+  "println"
+  "printf" 
+  "if"
+  "for" 
+  "while" 
+  "def" 
+] @keyword
+[ "+" "-" "*" "/" "**" ] @operator
+[ "<" ">" "<=" ">=" "==" "!=" "!" ] @operator
+"=" @operator
+"#" @operator
+
+(comment) @comment
+(identifier) @type
+(number) @number
+(function_definition (identifier) @function)
+
